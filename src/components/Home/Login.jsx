c89aaee326a2573392ac2b3a8cd89de6d555e24f
Farjana <farjanajhn12@gmail.com>
2023-05-18 07:00:55 +0600

    added a log in form in login page

--- src/components/Home/Login.jsx
+++ src/components/Home/Login.jsx
@@ -3,7 +3,36 @@
 const Login = () => {
   return (
     <div>
-      <h1>Login page is here</h1>
+ <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <p className="py-6">Login for get amazing offer and grab your faviroute lego set</p>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" name="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" name="password" className="input input-bordered" />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
