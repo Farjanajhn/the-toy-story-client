275ca7bf5039fdce4be2903631152b0b2365dcf2
Farjana <farjanajhn12@gmail.com>
2023-05-18 07:06:27 +0600

    registration form added in registration page

--- src/components/Home/Registration.jsx
+++ src/components/Home/Registration.jsx
@@ -2,7 +2,48 @@
 const Registration = () => {
   return (
     <div>
-      <h1>this is registration page</h1>
+     <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold text-center mb-2">Registration now!</h1>
+     <img src="https://www.leadquizzes.com/wp-content/uploads/2019/06/New-blog-graphic-16.jpg" alt="" />
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" placeholder="email" name="name" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">PhotoUrl</span>
+          </label>
+          <input type="text" placeholder="email" name="photo" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" name="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" name="password" className="input input-bordered" />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
