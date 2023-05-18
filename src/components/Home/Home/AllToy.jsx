36075285d0ebcb6bd661ca9ffa106ff245d05412
Farjana <farjanajhn12@gmail.com>
2023-05-19 03:35:21 +0600

    added the functionality of allToys route

--- src/components/Home/Home/AllToy.jsx
+++ src/components/Home/Home/AllToy.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import ShowToyes from "./ShowToyes";
 
 
 const AllToy = () => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     fetch('http://localhost:4000/products')
       .then(res => res.json())
@@ -11,8 +12,31 @@ const AllToy = () => {
   })
   return (
     <div>
-      <h1>toy</h1>
-    </div>
+      <div className="overflow-x-auto">
+  <table className="table table-compact w-full">
+    <thead>
+      
+      
+        <th>Name</th> 
+        <th>Toy Name</th> 
+        <th>Category</th> 
+        <th>Price</th> 
+        <th>Available Quantity</th> 
+        <th></th>
+   
+    </thead> 
+          <tbody>
+          {
+        products.map(product=><ShowToyes key={product._id} product={product}></ShowToyes>)
+    }
+    </tbody>
+    
+   
+  
+  </table>
+</div>
+  </div>
+
   );
 };
 
---
+++ src/components/Home/Home/ShowToyes.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+
+const ShowToyes = ({ product }) => {
+  const {_id,toyName,name,category,price,quantity}=product
+  return (
+
+
+
+      <tr>
+              
+              <td>{name }</td> 
+              <td>{toyName}</td> 
+              <td>{category}</td> 
+              <td>{price}</td> 
+              <td>{quantity}</td> 
+              <td>
+              <button className="btn btn-outline btn-primary"><Link to={`/products/${_id}`}>View Details</Link></button>
+              </td>
+      </tr>
+  
+ 
+  
+  );
+};
+
+export default ShowToyes;
